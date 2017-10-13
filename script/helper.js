914178c11d9ef30a01c889b313aaa5aaa2375041
Carmine Zaccagnino <carmine@PC-Carmine.localdomain>
2017-10-13 17:55:05 +0200

    Aggiunta selezione tra vari tipi di classifica

--- script/helper.js
+++ script/helper.js
@@ -44,8 +44,19 @@
   nomisquadre[lazio] = "Lazio";
   nomisquadre[udinese] = "Udinese";
 }).call(this);
+
+let tipoclassifica = () => {
+  let Alt = document.getElementById("alt").checked;
+  let Trad = document.getElementById("trad").checked;
+  let Somma = document.getElementById("somma").checked;
+  if (Alt) return "Alt";
+  if (Trad) return "Trad";
+  if (Somma) return "Somma";
+};
+
 function generaarray() {
-  (function () {
+  let classif = tipoclassifica();
+  if (classif == "Alt") {
     arr[inter] = Inter.getPunti();
     arr[juve] = Juve.getPunti();
     arr[milan] = Milan.getPunti();
@@ -66,9 +77,53 @@ function generaarray() {
     arr[sassuolo] = Sassuolo.getPunti();
     arr[lazio] = Lazio.getPunti();
     arr[udinese] = Udinese.getPunti();
-    sortedarr = Array.from(arr);
-    sortedarr.sort((a, b) => b - a);
-  }).call(this);
+  }
+  if (classif == "Trad") {
+    arr[inter] = Inter.getPuntiTrad();
+    arr[juve] = Juve.getPuntiTrad();
+    arr[milan] = Milan.getPuntiTrad();
+    arr[sampdoria] = Sampdoria.getPuntiTrad();
+    arr[torino] = Torino.getPuntiTrad();
+    arr[roma] = Roma.getPuntiTrad();
+    arr[benevento] = Benevento.getPuntiTrad();
+    arr[hellas] = Hellas.getPuntiTrad();
+    arr[atalanta] = Atalanta.getPuntiTrad();
+    arr[spal] = Spal.getPuntiTrad();
+    arr[crotone] = Crotone.getPuntiTrad();
+    arr[chievo] = Chievo.getPuntiTrad();
+    arr[fiorentina] = Fiorentina.getPuntiTrad();
+    arr[napoli] = Napoli.getPuntiTrad();
+    arr[bologna] = Bologna.getPuntiTrad();
+    arr[cagliari] = Cagliari.getPuntiTrad();
+    arr[genoa] = Genoa.getPuntiTrad();
+    arr[sassuolo] = Sassuolo.getPuntiTrad();
+    arr[lazio] = Lazio.getPuntiTrad();
+    arr[udinese] = Udinese.getPuntiTrad();
+  }
+  if (classif == "Somma") {
+    arr[inter] = Inter.getPunti() + Inter.getPuntiTrad();
+    arr[juve] = Juve.getPunti() + Juve.getPuntiTrad();
+    arr[milan] = Milan.getPunti() + Milan.getPuntiTrad();
+    arr[sampdoria] = Sampdoria.getPunti() + Sampdoria.getPuntiTrad();
+    arr[torino] = Torino.getPunti() + Torino.getPuntiTrad();
+    arr[roma] = Roma.getPunti() + Roma.getPuntiTrad();
+    arr[benevento] = Benevento.getPunti() + Benevento.getPuntiTrad();
+    arr[hellas] = Hellas.getPunti() + Hellas.getPuntiTrad();
+    arr[atalanta] = Atalanta.getPunti() + Atalanta.getPuntiTrad();
+    arr[spal] = Spal.getPunti() + Spal.getPuntiTrad();
+    arr[crotone] = Crotone.getPunti() + Crotone.getPuntiTrad();
+    arr[chievo] = Chievo.getPunti() + Chievo.getPuntiTrad();
+    arr[fiorentina] = Fiorentina.getPunti() + Fiorentina.getPuntiTrad();
+    arr[napoli] = Napoli.getPunti() + Napoli.getPuntiTrad();
+    arr[bologna] = Bologna.getPunti() + Bologna.getPuntiTrad();
+    arr[cagliari] = Cagliari.getPunti() + Cagliari.getPuntiTrad();
+    arr[genoa] = Genoa.getPunti() + Genoa.getPuntiTrad();
+    arr[sassuolo] = Sassuolo.getPunti() + Sassuolo.getPuntiTrad();
+    arr[lazio] = Lazio.getPunti() + Lazio.getPuntiTrad();
+    arr[udinese] = Udinese.getPunti() + Udinese.getPuntiTrad();
+  }
+  sortedarr = Array.from(arr);
+  sortedarr.sort((a, b) => b - a);
 }
 
 let failista = function () {
