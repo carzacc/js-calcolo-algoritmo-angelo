766c9ac264f0577e0c3dfd04b36c204fa8e445b9
Carmine Zaccagnino <carmine@PC-Carmine.localdomain>
2017-10-15 09:00:05 +0200

    Reso tutto piu chiaro

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,17 @@
+/*
+    Copyright (C) 2017 carzacc
+    This file is part of classifica-serie-a-alternativa.
+    classifica-serie-a-alternativa is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    classifica-serie-a-alternativa is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with classifica-serie-a-alternativa.  If not, see <http://www.gnu.org/licenses/>.
+*/
 'use strict';
   (function() {
     squadre[inter] = Inter;
