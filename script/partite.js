31dfb24154900f2b34737210fbada6787f3e4fd3
Carmine <zaccagnino@hotmail.it>
2017-10-28 12:10:21 +0200

    Update partite.js

--- script/partite.js
+++ script/partite.js
@@ -119,6 +119,16 @@ function partite(giornata) {
         partita("Torino","Roma",0,1);
         partita("Udinese","juventus",2,6);
         partita("Lazio","Cagliari",3,0);
+partita("Inter","Sampdoria",3,2);
+partita("Atalanta","Verona",3,0);
+partita("Bologna","Lazio",1,2);
+partita("Cagliari", "Benevento",2,1);
+partita("Chievo","Milan",1,4);
+partita("Fiorentina","Torino",3,0);
+partita("Genoa","Napoli",2,3);
+partita("juventus","Spal",4,1);
+partita("Roma","Crotone",1,0);
+partita("Sassuolo","Udinese",0,1);
       }
     }
   }
